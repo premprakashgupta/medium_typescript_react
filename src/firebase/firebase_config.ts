a8fd90f138096df7decc9d3054a35e97cf0a5435
PREM PRAKASH GUPTA <72307003+premprakashgupta@users.noreply.github.com>
2023-06-11 15:56:01 +0530

    Update firebase_config.ts change api for firebase

--- src/firebase/firebase_config.ts
+++ src/firebase/firebase_config.ts
@@ -6,14 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDVUPjvAM8NZBM2Zcy8rUFNtOigP8CVI74",
-  authDomain: "attendance-336da.firebaseapp.com",
-  projectId: "attendance-336da",
-  storageBucket: "attendance-336da.appspot.com",
-  messagingSenderId: "112594904814",
-  appId: "1:112594904814:web:eceb2812c839d73fa7f5ff",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
